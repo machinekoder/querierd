0b5bd995047057fcc6a8e55bff691bbd60cda730
Alexander Rössler <mail.aroessler@gmail.com>
2016-01-22 21:45:16 -0600

    updated querierd to work on all interfaces

--- querier/__init__.py
+++ querier/__init__.py
@@ -17,7 +17,12 @@
 # You should have received a copy of the GNU General Public License
 # along with QuerierD.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, sys, socket, time, struct, threading, syslog, signal
+import os
+import socket
+import time
+import struct
+import threading
+import syslog
 from .packets import IPv4Packet, IGMPv2Packet
 
 version = '0.1'
@@ -25,6 +30,7 @@ __all__ = ['Querier']
 
 all_routers = '224.0.0.1'
 mdns_group = '224.0.0.251'
+
 
 class Querier:
     """
@@ -38,19 +44,16 @@ class Querier:
         self.socket = sock = socket.socket(socket.AF_INET,
                                            socket.SOCK_RAW,
                                            socket.IPPROTO_RAW)
-        time.sleep(1) # Can't set options too soon (???)
+        time.sleep(1)  # Can't set options too soon (???)
         sock.settimeout(5)
         sock.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
-        sock.bind((source_address,0))
+        sock.bind((source_address, 0))
         self.build_query_packet()
         self.listener = None
         self.elected = True
-        self.stop = False
+        self.stop = threading.Event()
         syslog.openlog('querierd')
-        def sigterm_handler(signal, frame):
-            self.stop = True
-        signal.signal(signal.SIGTERM, sigterm_handler)
-        
+
     def build_query_packet(self):
         igmp = IGMPv2Packet()
         igmp.type = 'query'
@@ -63,30 +66,46 @@ class Querier:
         ip.src = self.source_address
         ip.dst = all_routers
         ip.data = igmp
-    
+
     def run(self):
-        syslog.syslog('Querier starting.')
+        syslog.syslog('Querier starting. %s' % self.source_address)
+        wait = 0.0
+        timeout = 0.5
         self.listener = QueryListener(self.source_address)
+
         while True:
+            if self.stop.is_set():
+                break
+
+            time.sleep(timeout)
+            wait += timeout
+            if wait < self.interval:
+                continue
+            else:
+                wait = 0.0
+
             elapsed = self.listener.elapsed()
             if self.elected:
                 self.socket.sendto(str(self.packet), (all_routers, 0))
                 if elapsed < self.interval:
                     self.elected = False
-                    syslog.syslog('Lost querier election. Pausing.')
+                    syslog.syslog('Lost querier election. Pausing. %s'
+                                  % self.source_address)
             else:
-                if elapsed > 2*self.interval:
-                    syslog.syslog('Won querier election. Resuming.')
+                if (elapsed > 2 * self.interval):
+                    syslog.syslog('Won querier election. Resuming. %s'
+                                  % self.source_address)
                     self.elected = True
-            if self.stop:
-                break
             if not self.listener.thread.is_alive():
-                syslog.syslog('Listener thread died.  Quitting.')
-                sys.exit(1)
-            time.sleep(self.interval)
+                syslog.syslog('Listener thread died.  Quitting. %s'
+                              % self.source_address)
+                break
+
+        self.listener.stop.set()
         self.socket.close()
-        syslog.syslog('Received SIGTERM.  Quitting.')
-        sys.exit(0)
+        syslog.syslog('Received SIGTERM.  Quitting. %s'
+                      % self.source_address)
+
 
 class QueryListener:
     """
@@ -95,29 +114,36 @@ class QueryListener:
     """
     def __init__(self, address):
         self.address = self._ip_as_int(address)
-        self._timestamp = 0 # the timestamp is shared data
+        self._timestamp = 0  # the timestamp is shared data
         self.socket = sock = socket.socket(socket.AF_INET,
                                     socket.SOCK_RAW,
                                     socket.IPPROTO_IGMP)
         sock.bind(('224.0.0.1', 0))
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.settimeout(0.5)  # timeout for stopping thread
         self.lock = threading.Lock()
+        self.stop = threading.Event()
         self.thread = thread = threading.Thread(target=self.listen)
-        thread.daemon = True
+        #thread.daemon = True
         thread.start()
 
     def _ip_as_int(self, address):
         return struct.unpack("!I", socket.inet_aton(address))[0]
-        
+
     def listen(self):
-        while True:
-            data, address = self.socket.recvfrom(65565)
-            if ord(data[20]) == 17: # make sure we got a query packet
+        while not self.stop.is_set():
+            try:
+                data, address = self.socket.recvfrom(65565)
+            except socket.timeout:
+                continue
+
+            if ord(data[20]) == 17:  # make sure we got a query packet
                 if self._ip_as_int(address[0]) < self.address:
                     self.lock.acquire()
                     self._timestamp = time.time()
                     self.lock.release()
-    
+        self.socket.close()
+
     def elapsed(self):
         """
         Return the time elapsed since receiving a query from a
--- querier/service.py
+++ querier/service.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright© 2014 by Marc Culler and others.
+# Copyright© 2016 by Alexander Roessler.
+# Based on the work of Mark Culler and others.
 # This file is part of QuerierD.
 #
 # QuerierD is free software: you can redistribute it and/or modify
@@ -9,7 +10,7 @@
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 #
-# QuerierD is distributed in the hope that it will be useful,
+# QuerierD is distributed in the hope that it will be useful
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
@@ -17,30 +18,92 @@
 # You should have received a copy of the GNU General Public License
 # along with QuerierD.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, netifaces
-from ConfigParser import ConfigParser
 from . import Querier
+import threading
+import time
+import sys
+import argparse
+import os
+from netifaces import interfaces, ifaddresses, AF_INET
 
-config_file = '/etc/querierd'
+
+class QuerierInstance:
+    def __init__(self, address, interval):
+        self.address = address
+        self.interval = interval
+        self.querier = Querier(address, interval)
+        self.thread = thread = threading.Thread(target=self.run)
+        thread.start()
+
+    def run(self):
+        self.querier.run()
+
+    def stop(self):
+        self.querier.stop.set()
+
+
+def ip4_addresses():
+    ip_list = []
+    for interface in interfaces():
+        if interface == 'lo':
+            continue
+        addresses = ifaddresses(interface)
+        if AF_INET in addresses:
+            for link in addresses[AF_INET]:
+                ip_list.append(link['addr'])
+    return ip_list
+
 
 def main():
+    parser = argparse.ArgumentParser(description='Querierd queries the multicast group in a certain interval to prevent IGMP snooping')
+    parser.add_argument('-i', '--interval', help='IGMP query interval', default=60.0)
+    parser.add_argument('-d', '--debug', help='Enable debug mode', action='store_true')
+    args = parser.parse_args()
+
     if os.getuid() != 0:
         print 'You must be root to run a querier.'
         sys.exit(1)
 
-    config = ConfigParser()
-    config.read(config_file)
-    query_interval = int(config.get('querierd', 'query_interval'))
-    interface = config.get('querierd', 'interface')
+    debug = args.debug
+    interval = args.interval
+    wait = 5.0  # network interface checking interval
+    processes = {}
+
     try:
-        ip = netifaces.ifaddresses(interface)[netifaces.AF_INET][0]['addr']
-    except ValueError:
-        raise ValueError(
-            'Interface %s not found. Please check %s .'%(interface, config_file)
-        )
-    print 'querier service starting. Using address %s'%ip
-    Querier(ip, query_interval).run()
+        while True:
+            addresses = ip4_addresses()
+            for address in addresses:
+                if address not in processes:
+                    if debug:
+                        print('adding new querier: %s' % address)
+                    processes[address] = QuerierInstance(address, interval)
+
+            removed = []
+            for proc in processes:
+                if proc not in addresses:
+                    if debug:
+                        print('stopping querier: %s' % proc)
+                    processes[proc].stop()
+                    removed.append(proc)
+            for proc in removed:
+                processes.pop(proc)
+
+            time.sleep(wait)
+    except KeyboardInterrupt:
+        pass
+
+    if debug:
+        print("stopping threads")
+    for proc in processes:
+        processes[proc].stop()
+
+    # wait for all threads to terminate
+    while threading.active_count() > 1:  # one thread for every process is left
+        time.sleep(0.1)
+
+    if debug:
+        print("threads stopped")
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
-
